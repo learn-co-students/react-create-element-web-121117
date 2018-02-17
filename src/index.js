46c40a3331b8d0cdb4547620734633563153d70f
julientregoat <julienctregoat@gmail.com>
2018-02-17 09:05:57 -0500

    done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-// Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = React.createElement('h1', {}, 'My First React Code')
+const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!')
+
+const list =
+  React.createElement('div', {},
+    React.createElement('h2', {}, 'My favorite ice cream flavors'),
+    React.createElement('ul', {},
+      [
+        React.createElement('li', {className : 'brown'}, 'Chocolate'),
+        React.createElement('li', {className: 'white'}, 'Vanilla'),
+        React.createElement('li', {className: 'yellow'}, 'Strawberry')
+      ]
+    )
+  );
+
+const container = React.createElement('div', {}, [title, paragraph, list])
+
+ReactDOM.render(container, document.getElementById('global'))
